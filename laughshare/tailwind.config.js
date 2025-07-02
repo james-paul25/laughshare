8b6634a7452f8246bce965a0ce2f4bf92e12739a
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-07-02 22:12:27 +0800

    update

--- laughshare/tailwind.config.js
+++ laughshare/tailwind.config.js
@@ -9,3 +9,5 @@ export default {
   },
   plugins: [],
 }
+// This file is used to configure Tailwind CSS for the project.
+// It specifies the files to scan for class names and extends the default theme.    

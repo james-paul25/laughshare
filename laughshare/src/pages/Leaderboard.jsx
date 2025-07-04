c0bcef2286a947b80202959ac49ec9c2b7601d2d
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-07-04 14:00:29 +0800

    update daw ag leaderboard pero hinay kayo netttt sshiabl

--- laughshare/src/pages/Leaderboard.jsx
+++ laughshare/src/pages/Leaderboard.jsx
@@ -1,11 +1,33 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import useAuth from "../contexts/useAuth"
+import db from "../firebase"
 
 export default function Leaderboard(){
+    const {user} = useAuth()
+    const {jokes, setJokes} = useState([])
+
+    useEffect(() => {
+        const fetchJokes = async () => {
+            const jokeCollection = await getDocs(collection(db, "jokes"));
+            setJokes(
+                jokeCollection.docs.map((doc) => ({
+                id: doc.id,
+                likedBy: [],
+                ...doc.data(),
+                }))
+            );
+        } 
+    }, [])
+
 
     return(
-        <div className="flex flex-col items-center mb-4">
-            <h1>Leadeboard</h1>
-            <p className="text-sm text-gray-500 mt-2">Who got more more laugh ?</p>
+        <div>
+            <div className="flex flex-col items-center mb-4">
+                <h1>Leadeboard</h1>
+                <p className="text-sm text-gray-500 mt-2">Who got more more laugh ?</p>
+            </div>
+
+            <div></div>
         </div>
     )
 }
